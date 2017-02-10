e503d98c62e345074b0dd334c2436ddf3dc4aa50
Kai Curry <kai@cape.io>
2017-02-10 12:31:40 -0600

    SAVE_TRIPLE action

--- src/actions.js
+++ src/actions.js
@@ -2,7 +2,12 @@ import { createAction, noopAction } from 'cape-redux'
 
 export const AUTH = 'fire/AUTH'
 export const auth = noopAction(AUTH)
+
 export const LOGOUT = 'fire/LOGOUT'
 export const logout = noopAction(LOGOUT)
+
+export const SAVE_TRIPLE = 'fire/SAVE_TRIPLE'
+export const saveTriple = createSimpleAction(SAVE_TRIPLE, identity, nthArg(1))
+
 export const UPLOAD_FILE = 'fire/UPLOAD_FILE'
 export const uploadFile = createAction(UPLOAD_FILE)
