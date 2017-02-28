02751460da581720d7da97bffbd1aab5461bf8fe
Kai Curry <kai@cape.io>
2017-02-28 15:16:09 -0500

    reduxFirebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import initializeFirebase from './init'
 import middleware from './middleware'
+import storeListener from './storeListener'
 
 export * from './actions'
 export * from './util'
 export const fireMiddleware = middleware
-
+export const reduxFirebase = storeListener
 export default initializeFirebase
